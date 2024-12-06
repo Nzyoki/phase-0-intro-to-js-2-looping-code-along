1480205b6cf657804d75f8fc269355aa0e1689f9
Scolastica Nzyoki <solanzioki18@gmail.com>
2024-12-06 23:37:41 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const strings =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(strings){
+    const messages=[];
+    for(let i=0; i<strings.length; i++){
+        messages.push(`Thank you, ${strings[i]}, for the wonderful surprise gift!`);
+    }
+    return  messages;
+}
+
+function countDown(x){
+    while(x>=0){
+        console.log(x);
+        x--;
+    }
+}
